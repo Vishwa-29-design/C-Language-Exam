170fd89cb95fad3ee173cc0ec35bd05f9d5662ef
Vishwa-29-design <vidhwapatel92@gmail.com>
2025-12-02 13:28:13 +0530

    C Language Answer Files Uploaded

---
+++ Question 4.c
@@ -0,0 +1,21 @@
+// Question -> 4 : Read a character and print whether it is a vowe or consonant :
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+        printf("%c is a vowel.\n", ch);
+    }
+    else
+    {
+        printf("%c is a consonant.\n", ch);
+    }
+
+    return 0;
+}
